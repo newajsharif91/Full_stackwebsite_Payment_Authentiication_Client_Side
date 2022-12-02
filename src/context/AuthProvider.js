e2613187b270967a24ea21df6c4ab95b0dab732e
“[newajsharif91]” <[email>
2022-12-02 15:46:06 +0600

    check again

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -9,66 +9,61 @@ const provider = new GoogleAuthProvider()
 
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    // sign up or create user
-    const createUser = (email, password) => {
-        setLoading(true)
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+  // sign up or create user
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
+  // user login id set
+  const login = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    // user login
-    const login = (email, password) => {
-        setLoading(true)
-        return signInWithEmailAndPassword(auth, email, password)
-    }
+  // google login
+  const googleLogIn = () => {
+    return signInWithPopup(auth, provider);
+  };
 
+  // Logout
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-    // google login
-    const googleLogIn = () => {
-        return signInWithPopup(auth, provider)
-    }
+  // Update user
+  const updateUser = (userInfo) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, userInfo);
+  };
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      // console.log('user observing');
+      setUser(currentUser);
+      setLoading(false);
+    });
 
-    // Logout
-    const logOut = () => {
-        setLoading(true)
-        return signOut(auth)
-    }
+    return () => unsubscribe();
+  }, []);
 
-
-    // Update user 
-    const updateUser = (userInfo) =>{
-        setLoading(true)
-        return updateProfile(auth.currentUser, userInfo)
-    }
-
-
-    // const updateUser = (userInfo) =>{
-    //     setLoading(true)
-    //     return updateProfile(auth.currentUser, userInfo)
-    // }
-
-
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, currentUser => {
-            // console.log('user observing');
-            setUser(currentUser)
-            setLoading(false)
-        })
-
-        return () => unsubscribe()
-    }, [])
-
-
-    const authInfo = { createUser, login, googleLogIn, user, logOut, updateUser, loading, setLoading }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    )
+  const authInfo = {
+    createUser,
+    login,
+    googleLogIn,
+    user,
+    logOut,
+    updateUser,
+    loading,
+    setLoading,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
